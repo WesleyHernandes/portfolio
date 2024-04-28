c608814ecd443d9a4feb02c9d0879fbb838292bf
WesleyHernandes <wess.hernandes@gmail.com>
2024-04-28 15:04:03 -0300

    Componente "Section"

---
+++ src/components/Section.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Section, { SectionProps } from "./Section";
+
+export default {
+  title: "Section",
+  component: Section,
+  args: {
+    position: "ONE",
+  },
+  argTypes: {
+    position: {
+      control: {
+        type: "inline-radio",
+      },
+    },
+  },
+} satisfies Meta<SectionProps>;
+
+export const One: StoryObj<SectionProps> = {
+  args: {
+    position: "ONE",
+  },
+};
+
+export const Two: StoryObj<SectionProps> = {
+  args: {
+    position: "TWO",
+  },
+};
+
+export const Three: StoryObj<SectionProps> = {
+  args: {
+    position: "TRHEE",
+  },
+};
---
+++ src/components/Section.tsx
@@ -0,0 +1,43 @@
+export interface SectionProps {
+  position: "ONE" | "TWO" | "TRHEE";
+  children: React.ReactNode;
+}
+
+export default function Section({ children, position }: SectionProps) {
+  const getPosition = () => {
+    switch (position) {
+      case "ONE":
+        return {
+          before: "bottom-0 -translate-x-1/3 translate-y-1/3",
+          after: "top-0 translate-x-1/2 -translate-y-1/2",
+        };
+      case "TWO":
+        return {
+          before: "bottom-0 -translate-x-1/3 translate-y-1/3",
+          after: "top-0 translate-x-1/2 translate-y-1/2",
+        };
+      case "TRHEE":
+        return {
+          before: "top-0 -translate-x-1/3 -translate-y-1/3",
+          after: "bottom-0 translate-x-1/2 translate-y-1/2",
+        };
+    }
+  };
+
+  return (
+    <section className="overflow-hidden relative flex flex-col w-full min-h-screen py-100 bg-[var(--dark)]">
+      <div
+        className={`absolute flex w-[44%] left-0 aspect-square opacity-50 bg-[var(--medium)] rounded-full ${
+          getPosition().before
+        }`}
+      />
+      <div
+        className={`absolute flex w-[34%] right-0 aspect-square opacity-50 bg-[var(--medium)] rounded-full ${
+          getPosition().after
+        }`}
+      />
+
+      {children}
+    </section>
+  );
+}
